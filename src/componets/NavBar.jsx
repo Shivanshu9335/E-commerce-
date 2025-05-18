836fdabed82fafca63b36a12ef819ce72c960c4f
@shivanshu_singh <shivanshuji50@gmail.com>
2025-05-18 19:59:44 +0530

    Code in responsive mode

--- src/componets/NavBar.jsx
+++ src/componets/NavBar.jsx
@@ -6,34 +6,24 @@ import { VscThreeBars } from "react-icons/vsc";
 import { CiSearch } from "react-icons/ci";
 import { toast } from "react-toastify";
 import { Link } from "react-router-dom";
-import { Login } from "@mui/icons-material";
 import { ChangeRouter } from "../store/LoginSlice";
 
 const Navbar = ({ search }) => {
-  // use the log-out page ---->>>
-  let login1 = useSelector((slice) => slice.Login);
-  console.log(login1)
-  let login = (login1.getValue.Login);
+  const login1 = useSelector((slice) => slice.Login);
+  const login = login1.getValue.Login;
+  const dispatch = useDispatch();
 
-  let dispatch = useDispatch()
-  const handleCLick = ()=>{
-     dispatch(ChangeRouter({email:'',Login:false}));
-       localStorage.setItem(
-         "Login",
-         JSON.stringify({ email: "", Login: false })
-       );
-  }
+  const handleCLick = () => {
+    dispatch(ChangeRouter({ email: "", Login: false }));
+    localStorage.setItem("Login", JSON.stringify({ email: "", Login: false }));
+  };
 
   const cartItems = useSelector((state) => state.cart.cartArr.length);
-  // console.log(cartItems)
   const wishItems = useSelector((state) => state.wish.value.length);
 
-
-  //Responesive toggle --->>>>
   const [showMenu, setShowMenu] = useState(false);
   const inputRef = useRef();
 
-  // Search the values--->>>>
   const handleSubmit = (e) => {
     e.preventDefault();
     const searchValue = inputRef.current.value.trim();
@@ -79,7 +69,7 @@ const Navbar = ({ search }) => {
               </span>
             )}
           </Link>
-          <Link to={"/cart"} className="  relative">
+          <Link to={"/cart"} className="relative">
             <AiOutlineShoppingCart size={26} className="text-indigo-200" />
             {cartItems > 0 && (
               <span className="absolute -top-2 -right-2 bg-blue-600 text-white text-xs px-1 rounded-full">
@@ -87,33 +77,35 @@ const Navbar = ({ search }) => {
               </span>
             )}
           </Link>
+
+          {/* Show Login/Signup on large screens */}
           {login === false && (
-            <li>
-              <Link
-                to={"/login"}
-                className="px-4   font-serif  border-2 border-gray-300 py-1.5 bg-fuchsia-500 text-black rounded-lg hover:bg-cyan-400"
-              >
-                Login
-              </Link>
-            </li>
-          )}
-          {login === false && (
-            <li>
-              <Link
-                to={"/signup"}
-                className="px-2  font-serif border-2 border-gray-300   py-1.5 bg-yellow-300 text-black rounded-lg hover:bg-yellow-400"
-              >
-                Sign Up
-              </Link>
-            </li>
+            <>
+              <li className="hidden md:block">
+                <Link
+                  to={"/login"}
+                  className="px-4 font-serif border-2 border-gray-300 py-1.5 bg-fuchsia-500 text-black rounded-lg hover:bg-cyan-400"
+                >
+                  Login
+                </Link>
+              </li>
+              <li className="hidden md:block">
+                <Link
+                  to={"/signup"}
+                  className="px-2 font-serif border-2 border-gray-300 py-1.5 bg-yellow-300 text-black rounded-lg hover:bg-yellow-400"
+                >
+                  Sign Up
+                </Link>
+              </li>
+            </>
           )}
 
+          {/* Show Logout on large screen only */}
           {login === true && (
-            <li>
+            <li className="hidden md:block">
               <p
                 onClick={handleCLick}
-                to={"/signup"}
-                className="px-2   md:block lg:block hidde font-serif border-2 border-gray-300   py-1.5 bg-yellow-300 text-black rounded-lg hover:bg-yellow-400"
+                className="px-2 font-serif border-2 border-gray-300 py-1.5 bg-yellow-300 text-black rounded-lg hover:bg-yellow-400 cursor-pointer"
               >
                 LogOut
               </p>
@@ -134,8 +126,8 @@ const Navbar = ({ search }) => {
       {showMenu && (
         <div className="md:hidden bg-gray-900 text-white p-4 absolute top-full w-full">
           <ul className="flex flex-col space-y-3">
-            <form onSubmit={handleSubmit} action="">
-              <li className="bg-cyan-300 text-black rounded-2xl text-center ">
+            <form onSubmit={handleSubmit}>
+              <li className="bg-cyan-300 text-black rounded-2xl text-center">
                 <input
                   ref={inputRef}
                   type="text"
@@ -144,33 +136,35 @@ const Navbar = ({ search }) => {
                 />
               </li>
             </form>
+
+            {/* Show Login/Signup on small screens */}
             {login === false && (
-              <li>
-                <Link
-                  to={"/login"}
-                  className="px-4   font-serif  border-2 border-gray-300 py-1.5 bg-fuchsia-500 text-black rounded-lg hover:bg-cyan-400"
-                >
-                  Login
-                </Link>
-              </li>
+              <>
+                <li>
+                  <Link
+                    to={"/login"}
+                    className="px-4 font-serif border-2 border-gray-300 py-1.5 bg-fuchsia-500 text-black rounded-lg hover:bg-cyan-400"
+                  >
+                    Login
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to={"/signup"}
+                    className="px-2 font-serif border-2 border-gray-300 py-1.5 bg-yellow-300 text-black rounded-lg hover:bg-yellow-400"
+                  >
+                    Sign Up
+                  </Link>
+                </li>
+              </>
             )}
 
-            {login === false && (
-              <li>
-                <Link
-                  to={"/signup"}
-                  className="px-2  font-serif border-2 border-gray-300   py-1.5 bg-yellow-300 text-black rounded-lg hover:bg-yellow-400"
-                >
-                  Sign Up
-                </Link>
-              </li>
-            )}
+            {/* Show Logout on small screens only */}
             {login === true && (
               <li>
                 <p
                   onClick={handleCLick}
-                  to={"/signup"}
-                  className="px-2  font-serif border-2 border-gray-300   py-1.5 bg-yellow-300 text-black rounded-lg hover:bg-yellow-400"
+                  className="px-2 font-serif border-2 border-gray-300 py-1.5 bg-yellow-300 text-black rounded-lg hover:bg-yellow-400 cursor-pointer"
                 >
                   LogOut
                 </p>
